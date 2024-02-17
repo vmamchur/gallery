542c10b54740eb17098a6eb66ff0bb6da20e2b0e
SimyachenkoAndriy <simyachenko.andriy@gmail.com>
2024-02-17 14:52:46 +0200

    style(client): fonts setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,14 @@ function App() {
   return (
     <div className={styles.App}>
       <div className={styles.section}>
-        <div className={styles.primary}>primary</div>
+        <div className={styles.primary}>PinInterest</div>
         <div className={styles.primary80}>primary80</div>
         <div className={styles.primary40}>primary40</div>
         <div className={styles.primary10}>primary10</div>
       </div>
 
       <div className={styles.section}>
-        <div className={styles.blackText}>blackText</div>
+        <div className={styles.blackText}>Whereas recognition</div>
         <div className={styles.grayLight}>grayLight</div>
         <div className={styles.grayMedium}>grayMedium</div>
         <div className={styles.grayDark}>grayDark</div>
