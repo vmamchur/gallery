4a21571fae9d9a0e09af653913f4b9d560881e29
SimyachenkoAndriy <simyachenko.andriy@gmail.com>
2024-02-17 13:40:58 +0200

    style(colors): color system has beem implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,19 @@ import styles from "./App.module.scss";
 function App() {
   return (
     <div className={styles.App}>
-      <h1 className={styles.text}>ewewewewe</h1>
+      <div className={styles.section}>
+        <div className={styles.primary}>primary</div>
+        <div className={styles.primary80}>primary80</div>
+        <div className={styles.primary40}>primary40</div>
+        <div className={styles.primary10}>primary10</div>
+      </div>
+
+      <div className={styles.section}>
+        <div className={styles.blackText}>blackText</div>
+        <div className={styles.grayLight}>grayLight</div>
+        <div className={styles.grayMedium}>grayMedium</div>
+        <div className={styles.grayDark}>grayDark</div>
+      </div>
     </div>
   );
 }
