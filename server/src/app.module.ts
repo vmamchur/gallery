4c15c2bb2ec94f4ef1a0ecbc011a168dc8d8a67c
vmamchur <vmchr344@gmail.com>
2024-02-24 19:03:49 +0200

    feat(server: images): implement basic multer images handling

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-
-ConfigModule.forRoot();
+import { ImagesModule } from './images/images.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_CONNECTION!), UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION!),
+    MulterModule.register({ dest: process.env.UPLOAD_PATH }),
+    UsersModule,
+    AuthModule,
+    ImagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/images/schemas/image.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type ImageDocument = Image & Document;
+
+@Schema()
+export class Image {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  path: string;
+}
+
+export const ImageSchema = SchemaFactory.createForClass(Image);
