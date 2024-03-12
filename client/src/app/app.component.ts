39fd6390f3ffdfdea29d27724e24800566ccf027
vmamchur <vmchr344@gmail.com>
2024-03-12 16:41:40 +0200

    chore(client/icons): manage svg icons

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule],
+  imports: [RouterOutlet, SvgIconComponent, SharedModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
