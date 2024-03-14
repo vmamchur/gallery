d70cd722752b9f98ef7f902d8ed6c8d7941b40bb
vmamchur <vmchr344@gmail.com>
2024-03-14 15:48:50 +0200

    feat(client/header): add navigation between auth routes

--- client/src/app/core/components/header/header.component.ts
+++ client/src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 import { SharedModule } from '../../../shared/shared.module';
 import { LogoComponent } from '../logo/logo.component';
@@ -6,7 +7,13 @@ import { LogoComponent } from '../logo/logo.component';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SharedModule, LogoComponent],
+  imports: [SharedModule, LogoComponent, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  getAuthButtonVariant(route: '/sign-in' | '/sign-up') {
+    return this.router.url === route ? 'solid' : 'outline';
+  }
+}
