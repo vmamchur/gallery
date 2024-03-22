e850a5ee88779dc3afe8ec28ff5fccdf0666f844
vmamchur <vmchr344@gmail.com>
2024-03-23 00:59:11 +0200

    refactor(client/header): decompose auth buttons

--- client/src/app/pages/gallery.page.tsx
+++ client/src/app/pages/gallery.page.tsx
@@ -1,6 +1,6 @@
 import CreateImageButton from "@features/images/components/create-image-button.component";
 import ImageList from "@features/images/components/image-list.component";
-import Header from "@shared/components/header.component";
+import Header from "@shared/components/header/header.component";
 
 const Gallery = () => {
   return (
