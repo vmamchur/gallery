14217e66534d58791d44211db0b35a04505d3af6
vmamchur <vmchr344@gmail.com>
2024-03-28 23:35:38 +0200

    feat(client/image): implement basic ImageDetailsModal component

--- client/src/app/pages/gallery.page.tsx
+++ client/src/app/pages/gallery.page.tsx
@@ -1,4 +1,5 @@
 import CreateImageButton from "@features/images/components/create-image-button.component";
+import ImageDetailsModal from "@features/images/components/image-details-modal.component";
 import ImageList from "@features/images/components/image-list.component";
 import Header from "@shared/components/header/header.component";
 
@@ -8,6 +9,7 @@ const Gallery = () => {
       <Header />
       <ImageList />
       <CreateImageButton />
+      <ImageDetailsModal />
     </>
   );
 };
