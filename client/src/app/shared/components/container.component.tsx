baf1b72bef66ab4bf30c5b66eb6bec6f421ec55f
vmamchur <vmchr344@gmail.com>
2024-04-01 00:48:16 +0300

    change(client/container): increase padding top

--- client/src/app/shared/components/container.component.tsx
+++ client/src/app/shared/components/container.component.tsx
@@ -12,7 +12,7 @@ const Container: FC<ContainerProps> = ({ children }) => {
   return (
     <div
       className={classNames(
-        "h-screen lg:max-w-screen-2xl mx-auto pt-16 px-4 lg:px-8 flex flex-col",
+        "h-screen lg:max-w-screen-2xl mx-auto pt-[72px] px-4 lg:px-8 flex flex-col",
         { "overflow-y-hidden": activeModalName in EModalName },
       )}
     >
