ea73d9adde15a22e4dae0c6e46d73b5e302e8a6f
vmamchur <vmchr344@gmail.com>
2024-04-15 16:29:20 +0300

    feat(client/modal): close modal on esc

--- client/src/app/shared/components/modal-wrapper.components.tsx
+++ client/src/app/shared/components/modal-wrapper.components.tsx
@@ -15,31 +15,37 @@ const ModalWrapper: FC<ModalWrapperProps> = ({ children, isOpen }) => {
   };
 
   useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        dispatch(modalsActions.closeModal());
+      }
+    };
+
     if (isOpen) {
-      document.body.style.overflowY = 'hidden';
+      document.body.style.overflowY = "hidden";
+      document.addEventListener("keydown", handleKeyPress);
     }
 
     return () => {
-      document.body.style.overflowY = 'auto';
+      document.body.style.overflowY = "auto";
+      document.removeEventListener("keydown", handleKeyPress);
     };
-  }, [isOpen]);
-
-  if (!isOpen) {
-    return null;
-  }
+  }, [isOpen, dispatch]);
 
   return (
-    <div
-      className="min-h-screen fixed top-0 bottom-0 right-0 left-0 z-10 overflow-y-auto bg-black bg-opacity-50 flex justify-center items-center"
-      onClick={handleClose}
-    >
+    isOpen && (
       <div
-        className="w-full py-4 px-6 mx-10 lg:mx-32 border-2 border-black rounded-lg bg-white"
-        onClick={(event) => event.stopPropagation()}
+        className="min-h-screen fixed top-0 bottom-0 right-0 left-0 z-10 overflow-y-auto bg-black bg-opacity-50 flex justify-center items-center"
+        onClick={handleClose}
       >
-        {children}
+        <div
+          className="w-full py-4 px-6 mx-10 lg:mx-32 border-2 border-black rounded-lg bg-white"
+          onClick={(event) => event.stopPropagation()}
+        >
+          {children}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
