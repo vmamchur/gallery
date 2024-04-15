8b31b15a10c8c6c27f21e501c34cdef199858a99
vmamchur <vmchr344@gmail.com>
2024-04-15 16:53:07 +0300

    feat(client/create-image-form): reset preview image

--- client/src/app/features/images/components/image-preview.component.tsx
+++ client/src/app/features/images/components/image-preview.component.tsx
@@ -1,19 +1,35 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import ErrorMessage from "@shared/components/error-message.component";
+import ResetIcon from "../../../../assets/icons/trash.svg?react";
 
 type ImagePreviewProps = {
   image: string;
   error?: string;
+  onReset: () => void;
 };
 
-const ImagePreview: FC<ImagePreviewProps> = ({ image, error }) => {
+const ImagePreview: FC<ImagePreviewProps> = ({ image, error, onReset }) => {
+  const [isPreviewHovered, setIsPreviewHovered] = useState(false);
+
   return (
     <div>
-      <img
-        className="w-full h-full flex justify-center items-center"
-        src={image}
-      />
+      <div className="relative">
+        <img
+          className="w-full h-full flex justify-center items-center"
+          src={image}
+          onMouseEnter={() => setIsPreviewHovered(true)}
+        />
+        {isPreviewHovered && (
+          <button
+            className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center"
+            onClick={onReset}
+            onMouseLeave={() => setIsPreviewHovered(false)}
+          >
+            <ResetIcon width={200} height={200} />
+          </button>
+        )}
+      </div>
 
       {error && <ErrorMessage message={error} />}
     </div>
